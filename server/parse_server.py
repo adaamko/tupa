a41247b39a0da2bde1f208c8ffdcafa51e2b08b4
Kovács Ádám <adaam.ko@gmail.com>
2022-07-14 13:59:32 +0200

    Host change

--- server/parse_server.py
+++ server/parse_server.py
@@ -91,4 +91,4 @@ session_opts = {
 }
 
 if __name__ == "__main__":
-    app.run(debug=True, host='localhost', port=int(os.getenv("PORT", 5001)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT", 5001)))
